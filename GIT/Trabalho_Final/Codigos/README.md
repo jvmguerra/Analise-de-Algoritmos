0c1f738206d1f62c463d16e6bd022a6097f2c7fb
eddcp <eduardocspv@gmail.com>
2015-12-06 10:46:02 -0800

    marson gato

--- GIT/Trabalho_Final/Codigos/README.md
+++
@@ -1,1 +0,0 @@
- - Lembre de mudar o path nos arquivos teste(Nome_do_algoritmo).py
--- GIT/Trabalho_Final/Plot/plot_tempo.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from math import *
-
-n = np.arange(2, 2 ** 4)
-
-plt.plot(n, n, 'r--', n, n*np.log2(n), 'bs', n, n ** 2, 'g^')
-plt.show()
