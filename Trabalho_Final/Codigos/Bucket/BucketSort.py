40502d7f716bb01ee91c880c1343f8d520eca5c4
theffc <fredericoffc@gmail.com>
2015-12-16 18:39:35 -0200

    coloquei o Insertion dentro do Bucket, e tirei o numpy import do Insertion

--- Trabalho_Final/Codigos/Bucket/BucketSort.py
+++ Trabalho_Final/Codigos/Bucket/BucketSort.py
@@ -1,5 +1,5 @@
 
-
+@profile
 def bucket_sort(floats):
     buckets = [ [] for _ in range(len(floats)) ]
     for num in floats:
@@ -8,8 +8,19 @@ def bucket_sort(floats):
 
     result = []
     for bucket in buckets:
-        bucket.sort() # INSERTION_SORT(bucket)
+        _insertion_sort(bucket)# INSERTION_SORT(bucket)
         result += bucket
     return result
 
-print(bucket_sort([0.112, 0.3, 0.008, 0.07, 0.9, 0.8, 0.43, 0.29]))
+# colocamos a mesma versão do Insertion Sort, que já havíamos feito, aqui apenas
+# para facilitar a análise de complexidade do Bucket Sort
+def _insertion_sort(lista):
+	for j in range(1,len(lista)):
+		chave = lista[j]
+		i = j
+		while (i>0 and lista[i-1]>chave):
+			lista[i] = lista[i-1]
+			i = i-1
+		lista[i] = chave
+
+#print(bucket_sort([0.112, 0.3, 0.008, 0.07, 0.9, 0.8, 0.43, 0.29]))
